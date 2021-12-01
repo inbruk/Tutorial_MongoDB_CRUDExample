2e503799fcb73d4cbb024832a5295c9334b70311
inbruk <35333872+inbruk@users.noreply.github.com>
2021-12-01 19:27:58 +0300

    .

--- Demo/NameModel.cs
+++ Demo/NameModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace Demo
 {
     public class NameModel
     {
+        [BsonId]
+        public Guid Id { get; set; } // _id
+
         public String FirstName { set; get; }
 
         public String LastName { set; get; }
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -21,13 +21,14 @@ namespace Demo
             // crud.Create("Users", somePers);
             // Console.WriteLine("============================================================");
 
-            //var recs = crud.ReadAll<PersonModel>("Users");
-            //foreach (var item in recs)
-            //{
-            //    var iStr = JsonSerializer.Serialize(item);
-            //    Console.WriteLine(iStr);
-            //    Console.WriteLine("-----------------------------------------------------------");
-            //}
+            // var recs = crud.ReadAll<PersonModel>("Users");
+            var recs = crud.ReadAll<NameModel>("Users");
+            foreach (var item in recs)
+            {
+                var iStr = JsonSerializer.Serialize(item);
+                Console.WriteLine(iStr);
+                Console.WriteLine("-----------------------------------------------------------");
+            }
 
             //var rec = crud.Read<PersonModel>("Users", new Guid("7920d1c5-cc40-4e09-84b0-5b7d9c9b829b"));
             //var rStr = JsonSerializer.Serialize(rec);
