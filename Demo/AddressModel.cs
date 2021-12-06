bb7c8be1eeea74d868cdfd248ca8316a3923369d
inbruk <35333872+inbruk@users.noreply.github.com>
2021-12-06 14:36:19 +0300

    .

--- Demo/AddressModel.cs
+++ Demo/AddressModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo
+namespace Example
 {
     public class AddressModel
     {
--- Demo/NameModel.cs
+++ Demo/NameModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo
+namespace Example
 {
     public class NameModel
     {
